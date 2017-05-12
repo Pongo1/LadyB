a8de6fc5dec2d5d8d68e40e83bdde7b23a2a4307
Pongo1 <mrfimpong@gmail.com>
2017-05-12 02:25:34 +0200

    Everything except order and requests

---
+++ Pages/shop.js
@@ -0,0 +1,16 @@
+
+
+var f = function()
+{
+
+	alert("the 'ORDER' feature is not yet enabled. For now contact her assosciates via:(Whatsapp)+233209387773 to order. Follow Lady B on (Facebook): Lady Burgesson, (Instagram) Lady B\nEmail: CBurgesson16@alastudents.org");
+
+
+
+};
+
+
+var g= function() {
+
+	alert("The 'REQUESTS' feature is private");
+};
